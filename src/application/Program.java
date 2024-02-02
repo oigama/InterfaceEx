17a9d0864eab82bc7ed9a8d7cfcc513571a33b05
Matheus Gama <mathemovic.mg@gmail.com>
2024-02-02 09:32:40 -0300

    Primeira parte

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import entities.Contract;
+import entities.Installment;
+import services.ContractService;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -11,14 +13,14 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
 
-        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy.");
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         System.out.println("Entre com os dados do contrato:");
-        System.out.println("Numero: ");
+        System.out.print("Numero: ");
         int number = sc.nextInt();
-        System.out.println("Data (dd/MM/yyyy): ");
+        System.out.print("Data (dd/MM/yyyy): ");
         LocalDate date = LocalDate.parse(sc.next(), fmt);
-        System.out.println("Valor do contrato: ");
+        System.out.print("Valor do contrato: ");
         double totalValue = sc.nextDouble();
 
         Contract obj = new Contract(number, date, totalValue);
@@ -26,8 +28,16 @@ public class Program {
         System.out.println("Entre com o numero de parcelas: ");
         int n = sc.nextInt();
 
+        ContractService contractService = new ContractService(null);
+
+        contractService.processContract(obj, n);
 
 
+        System.out.print("Parcelas: ");
+
+        for (Installment installment : obj.getInstallments()) {
+            System.out.println(installment);
+        }
 
         sc.close();
 
--- src/services/ContractService.java
+++ src/services/ContractService.java
@@ -1,4 +1,23 @@
 package services;
 
+import entities.Contract;
+import entities.Installment;
+
+import java.time.LocalDate;
+
 public class ContractService {
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months) {
+
+        contract.getInstallments().add(new Installment(LocalDate.of(2024,7,25), 206.04));
+        contract.getInstallments().add(new Installment(LocalDate.of(2024,8,25), 208.08));
+
+    }
+
+
 }
