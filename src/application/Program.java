197f53c8a2cbf6535f35b63f2e3d6300856e84a8
Matheus Gama <mathemovic.mg@gmail.com>
2024-02-02 08:43:41 -0300

    Adicionando a Interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,35 @@
 package application;
 
+import entities.Contract;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy.");
+
+        System.out.println("Entre com os dados do contrato:");
+        System.out.println("Numero: ");
+        int number = sc.nextInt();
+        System.out.println("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        System.out.println("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+
+        Contract obj = new Contract(number, date, totalValue);
+
+        System.out.println("Entre com o numero de parcelas: ");
+        int n = sc.nextInt();
+
+
+
+
+        sc.close();
+
     }
 }
